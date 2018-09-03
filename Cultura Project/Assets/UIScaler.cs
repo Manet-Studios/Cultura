da95ee7e26365771c52420399bb9926cc514ee1a
Richard Palmer <ricky@rpgrafx.com>
2018-09-02 23:05:10 -0400

    Radial menu buttons grow on mouse over

---
+++ Cultura Project/Assets/UIScaler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScaler : MonoBehaviour
+{
+    private Vector3 originalSize;
+
+    [SerializeField]
+    private float sizeModifier;
+
+    private RectTransform rectTransform;
+
+    private void Start()
+    {
+        rectTransform = transform as RectTransform;
+        originalSize = rectTransform.localScale;
+    }
+
+    public void Grow()
+    {
+        rectTransform.localScale *= sizeModifier;
+    }
+
+    public void Shrink()
+    {
+        rectTransform.localScale = originalSize;
+    }
+}
