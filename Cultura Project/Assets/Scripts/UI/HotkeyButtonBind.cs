389a390ded3da4b20ec637d103ea2e97fa7b87ba
Richard Palmer <ricky@rpgrafx.com>
2018-09-04 20:54:42 -0400

    Hotkey button bind

---
+++ Cultura Project/Assets/Scripts/UI/HotkeyButtonBind.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Cultura.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class HotkeyButtonBind : MonoBehaviour
+    {
+        [SerializeField]
+        private KeyCode hotkey;
+
+        private Button button;
+
+        private void Start()
+        {
+            button = GetComponent<Button>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(hotkey))
+            {
+                button.onClick.Invoke();
+            }
+        }
+    }
+}
